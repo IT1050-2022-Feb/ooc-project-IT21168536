aee3083744623b6c3248fe0d391b5bc3f5d0f197
Chathura Lakshan <99183326+IT21168536@users.noreply.github.com>
2022-05-19 18:55:26 +0530

    Add files via upload

---
+++ seller.cpp
@@ -0,0 +1,12 @@
+//IT21168772 
+#include "Seller.h"
+
+seller::seller() {
+
+}
+void seller::DisplayDetails() {
+
+}
+seller::~seller() {
+
+}
---
+++ seller.h
@@ -0,0 +1,13 @@
+//IT21168772 
+#define size 2
+
+class seller {
+
+private:
+    int seller_ID;
+
+public:
+    seller();
+    void DisplayDetails();
+    ~seller();
+};
