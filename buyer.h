51587b5272e0ee141de95fe179999e27e54336a4
Chathura Lakshan <99183326+IT21168536@users.noreply.github.com>
2022-05-19 18:41:32 +0530

    Update buyer.h

--- buyer.h
+++ buyer.h
@@ -1,3 +1,4 @@
+//IT21168536
 #include "Person.h"
 #include "Property.h"
 #include <iostream>
@@ -13,7 +14,7 @@ class buyer : public Person
 	public:
 		buyer();
 		buyer(int B_ID, string B_Name, string C_Num, string eml);
-		void AddProperty(Property* prp);
+		void AddProperty(Property *prp);
 		void displayBuyer();
 		void Display();
 		~buyer();
